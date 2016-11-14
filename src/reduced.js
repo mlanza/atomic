753235d9e2478056d6817eba4711cc246ef1bb90
Mario T. Lanza <mlanza@doesideas.com>
2016-11-14 10:10:38 -0500

    add deref

--- src/reduced.js
+++ src/reduced.js
@@ -1,3 +1,6 @@
+import {extend} from './protocol';
+import Deref from './protocols/deref';
+
 export function Reduced(value){
   this.value = value;
 }
@@ -13,5 +16,11 @@ export function reduced(value){
 export function isReduced(value){
   return value instanceof Reduced;
 }
+
+extend(Deref, {
+  deref: function(reduced){
+    return reduced.valueOf();
+  }
+}, Reduced);
 
 export default Reduced;
