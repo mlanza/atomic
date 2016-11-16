4e6f2d32ca5d4b14d554c0f7b29796cb7b0a9c95
Mario T. Lanza <me@mlanza.com>
2016-11-16 16:25:11 -0500

    drop former implementation

--- _src/compare.js
+++
@@ -1,8 +0,0 @@
-import {subj} from './core/function.js';
-//export {} from './core/compare.js'; 
-import * as compare from './core/compare.js';
-export const eq  = subj(compare.eq);
-export const gt  = subj(compare.gt);
-export const gte = subj(compare.gte);
-export const lte = subj(compare.lte);
-export const lt  = subj(compare.lt);
