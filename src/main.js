caf12f50733081d73ea04be81307bbb68faff93e
Mario T. Lanza <mlanza@doesideas.com>
2016-11-17 09:22:57 -0500

    add second,third

--- src/main.js
+++ src/main.js
@@ -5,16 +5,16 @@ import HTMLDocument from './types/html-document';
 import HTMLElement from './types/html-element';
 import IndexedSeq from './types/indexed-seq';
 import Reduce from './protocols/reduce';
-import {log, chain, pipe, add, juxt, query, fetch, get, assoc, hasKey, first, rest, nth, next, count, reduce, each, map, filter, find, satisfies, concat, toArray, toObject} from './curried';
-export {log, chain, pipe, add, juxt, query, fetch, get, assoc, hasKey, first, rest, nth, next, count, reduce, each, map, filter, find, satisfies, concat, toArray, toObject} from './curried';
+import {log, chain, pipe, add, juxt, query, fetch, get, assoc, hasKey, first, second, third, rest, nth, next, count, reduce, each, map, filter, find, satisfies, concat, toArray, toObject} from './curried';
+export {log, chain, pipe, add, juxt, query, fetch, get, assoc, hasKey, first, second, third, rest, nth, next, count, reduce, each, map, filter, find, satisfies, concat, toArray, toObject} from './curried';
 export const nums = new IndexedSeq([11,12,13,14], 1);
 export const letters = new IndexedSeq("grace");
 
 QUnit.test("IndexedSeq", function(assert){
   assert.equal(chain(letters, first), "g");
-  assert.equal(chain(letters, nth(2)), "a");
+  assert.equal(chain(letters, third), "a");
   assert.equal(chain(nums, first), 12);
-  assert.equal(chain(nums, nth(1)), 13);
+  assert.equal(chain(nums, second), 13);
   assert.equal(chain(nums, count), 3);
   assert.ok(chain(nums, satisfies(Reduce)));
   assert.equal(chain(nums, reduce(add, 0)), 39);
