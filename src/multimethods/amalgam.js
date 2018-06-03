1cbcfd860fb9cee0933aa829a77517eb9414400b
Mario T. Lanza <mlanza@doesideas.com>
2018-06-03 17:38:05 -0400

    don't expose 3-parameter amalgam fns in favor of reducing api

--- src/multimethods/amalgam.js
+++ src/multimethods/amalgam.js
@@ -1,5 +1,6 @@
 //An amalgam is something composed of disparate parts
 
+import {overload} from "../core";
 import {detect, each, mapa, compact} from "../types/lazyseq/concrete";
 import {reduced, reducing} from "../types/reduced";
 import {multimethod} from "../types/multimethod/construct";
@@ -12,16 +13,22 @@ import {signature, or} from "../predicates";
 import {apply} from "../types/function/concrete";
 import {ICloneable, IReduce, IKVReduce, IEvented, IHierarchy, IContent, ILookup, IAssociative, IMap, isSequential, isDescriptive} from "../protocols";
 
-export const has = multimethod();
-const _inject = multimethod();
-export const _yank = multimethod();
-export const transpose = multimethod(function(self, other){
-  return has(self, other) ? _yank(self, other) : _inject(self, other);
+const yank1 = multimethod(), yank2 = multimethod(), yank3 = multimethod();
+const has2 = multimethod(), has3 = multimethod();
+const inject2 = multimethod(), inject3 = multimethod();
+const transpose2 = multimethod(function(self, other){
+  return has2(self, other) ? yank2(self, other) : inject2(self, other);
+}), transpose3 = multimethod(function(self, key, value){
+  return has3(self, key, other) ? yank3(self, key, other) : inject3(self, key, other);
 });
+export const inject = overload(null, null, inject2, reducing(inject2));
+export const yank = overload(null, yank1, yank2, reducing(yank2));
+export const has = overload(null, null, has2, has3);
+export const transpose = overload(null, null, transpose2, transpose3);
 
 /* Date / When */
 
-IEvented.on(_inject.instance, signature(isDate, isWhen), function(self, when){
+IEvented.on(inject2.instance, signature(isDate, isWhen), function(self, when){
   return IReduce.reduce(IMap.keys(self), function(dt, key){
     const value = ILookup.lookup(when, key);
     return value == null ? dt : IAssociative.assoc(dt, key, value);
@@ -30,7 +37,7 @@ IEvented.on(_inject.instance, signature(isDate, isWhen), function(self, when){
 
 /* When / When */
 
-IEvented.on(_inject.instance, signature(isWhen, isWhen), function(self, other){
+IEvented.on(inject2.instance, signature(isWhen, isWhen), function(self, other){
   return IReduce.reduce(IMap.keys(self), function(memo, key){
     const value = ILookup.lookup(other, key);
     return value == null ? memo : IAssociative.assoc(memo, key, value);
@@ -39,20 +46,20 @@ IEvented.on(_inject.instance, signature(isWhen, isWhen), function(self, other){
 
 /* Element */
 
-IEvented.on(_yank.instance, signature(isElement), function(self){
-  return _yank(IHierarchy.parent(self), self);
+IEvented.on(yank1.instance, signature(isElement), function(self){
+  return yank2(IHierarchy.parent(self), self);
 });
 
 /* Element / Keys */
 
 const elementKeys = signature(isElement, isSequential);
 
-IEvented.on(_yank.instance, elementKeys, function(self, keys){
+IEvented.on(yank2.instance, elementKeys, function(self, keys){
   each(self.removeAttribute.bind(self), keys);
   return self;
 });
 
-IEvented.on(has.instance, elementKeys, function(self, keys){
+IEvented.on(has2.instance, elementKeys, function(self, keys){
   return IReduce.reduce(keys, function(memo, key){
     return memo ? self.hasAttribute(key) : reduced(memo);
   }, true);
@@ -64,7 +71,7 @@ const elementStyleValue = signature(isElement, function(name){
   return name === "style";
 }, null);
 
-IEvented.on(_inject.instance, elementStyleValue, function(self, key, styles){
+IEvented.on(inject3.instance, elementStyleValue, function(self, key, styles){
   each(function(style){
     const [key, value] = mapa(trim, compact(split(style, ":")));
     self.style[key] = value;
@@ -72,7 +79,7 @@ IEvented.on(_inject.instance, elementStyleValue, function(self, key, styles){
   return self;
 });
 
-IEvented.on(_yank.instance, elementStyleValue, function(self, key, styles){
+IEvented.on(yank3.instance, elementStyleValue, function(self, key, styles){
   each(function(style){
     const [key, value] = mapa(trim, compact(split(style, ":")));
     if (self.style[key] == value) {
@@ -82,7 +89,7 @@ IEvented.on(_yank.instance, elementStyleValue, function(self, key, styles){
   return self;
 });
 
-IEvented.on(transpose.instance, elementStyleValue, function(self, key, styles){
+IEvented.on(transpose3.instance, elementStyleValue, function(self, key, styles){
   each(function(style){
     const [key, value] = mapa(trim, compact(split(style, ":")));
     self.style[key] = self.style[key] == value ? "" : value;
@@ -90,7 +97,7 @@ IEvented.on(transpose.instance, elementStyleValue, function(self, key, styles){
   return self;
 });
 
-IEvented.on(has.instance, elementStyleValue, function(self, key, styles){
+IEvented.on(has3.instance, elementStyleValue, function(self, key, styles){
   return IReduce.reduce(mapa(trim, compact(split(styles, ";"))), function(memo, style){
     const [key, value] = mapa(trim, split(style, ":"))
     return memo ? self.style[key] == value : reduced(memo);
@@ -103,22 +110,22 @@ const elementClassValue = signature(isElement, function(name){
   return name === "class";
 }, null);
 
-IEvented.on(_inject.instance, elementClassValue, function(self, key, value){
-  each(self.classList._inject.bind(self.classList), value.split(" "));
+IEvented.on(inject3.instance, elementClassValue, function(self, key, value){
+  each(self.classList.add.bind(self.classList), value.split(" "));
   return self;
 });
 
-IEvented.on(_yank.instance, elementClassValue, function(self, key, value){
-  each(self.classList._yank.bind(self.classList), value.split(" "));
+IEvented.on(yank3.instance, elementClassValue, function(self, key, value){
+  each(self.classList.remove.bind(self.classList), value.split(" "));
   return self;
 });
 
-IEvented.on(transpose.instance, elementClassValue, function(self, key, value){
+IEvented.on(transpose3.instance, elementClassValue, function(self, key, value){
   each(self.classList.toggle.bind(self.classList), value.split(" "));
   return self;
 });
 
-IEvented.on(has.instance, elementClassValue, function(self, key, value){
+IEvented.on(has3.instance, elementClassValue, function(self, key, value){
   return IReduce.reduce(value.split(" "), function(memo, name){
     return memo ? self.classList.contains(name) : reduced(memo);
   }, true);
@@ -128,35 +135,35 @@ IEvented.on(has.instance, elementClassValue, function(self, key, value){
 
 const elementKeyValue = signature(isElement, isString, null);
 
-IEvented.on(_inject.instance, elementKeyValue, function(self, key, value){
+IEvented.on(inject3.instance, elementKeyValue, function(self, key, value){
   self.setAttribute(key, value);
   return self;
 });
 
-IEvented.on(_yank.instance, elementKeyValue, function(self, key, value){
+IEvented.on(yank3.instance, elementKeyValue, function(self, key, value){
   if (value == null || value == self.getAttribute(key)) {
     self.removeAttribute(key);
   }
   return self;
 });
 
-IEvented.on(has.instance, elementKeyValue, function(self, key, value){
+IEvented.on(has3.instance, elementKeyValue, function(self, key, value){
   return self.getAttribute(key) == value;
 });
 
-IEvented.on(transpose.instance, elementKeyValue, function(self, key, value){
+IEvented.on(transpose3.instance, elementKeyValue, function(self, key, value){
   self.getAttribute(key) == value ? self.removeAttribute(key) : self.setAttribute(key, value);
   return self;
 });
 
 const elementEventCallback = signature(isElement, isString, isFunction);
 
-IEvented.on(_inject.instance, elementEventCallback, function(self, key, f){
+IEvented.on(inject3.instance, elementEventCallback, function(self, key, f){
   self.addEventListener(key, f);
   return self;
 });
 
-IEvented.on(_yank.instance, elementEventCallback, function(self, key, f){
+IEvented.on(yank3.instance, elementEventCallback, function(self, key, f){
   self.delEventListener(key, f);
   return self;
 });
@@ -165,42 +172,42 @@ IEvented.on(_yank.instance, elementEventCallback, function(self, key, f){
 
 const elementAttrs = signature(isElement, isDescriptive);
 
-IEvented.on(_inject.instance, elementAttrs, function(self, obj){
-  return IKVReduce.reducekv(obj, _inject, self);
+IEvented.on(inject2.instance, elementAttrs, function(self, obj){
+  return IKVReduce.reducekv(obj, inject3, self);
 });
 
-IEvented.on(has.instance, elementAttrs, function(self, obj){
+IEvented.on(has2.instance, elementAttrs, function(self, obj){
   return IKVReduce.reducekv(obj, function(memo, key, value){
-    return memo ? has(self, key, value) : reduced(memo);
+    return memo ? has3(self, key, value) : reduced(memo);
   }, true);
 });
 
-IEvented.on(_yank.instance, elementAttrs, function(self, obj){
-  return IKVReduce.reducekv(obj, _yank, self);
+IEvented.on(yank2.instance, elementAttrs, function(self, obj){
+  return IKVReduce.reducekv(obj, yank3, self);
 });
 
-IEvented.on(transpose.instance, elementAttrs, function(self, obj){
-  return IKVReduce.reducekv(obj, transpose, self);
+IEvented.on(transpose2.instance, elementAttrs, function(self, obj){
+  return IKVReduce.reducekv(obj, transpose3, self);
 });
 
 /* Element / Text */
 
 const elementText = signature(isElement, isString);
 
-IEvented.on(has.instance, elementText, function(self, text){
+IEvented.on(has2.instance, elementText, function(self, text){
   return detect(function(node){
     return node.nodeType === Node.TEXT_NODE && node.data === text;
   }, IContent.contents(self));
 });
 
-IEvented.on(_inject.instance, elementText, function(self, text){
+IEvented.on(inject2.instance, elementText, function(self, text){
   self.appendChild(document.createTextNode(text));
   return self;
 });
 
-IEvented.on(_yank.instance, elementText, function(self, text){
-  const node = has(self, text);
-  node && self.delChild(node);
+IEvented.on(yank2.instance, elementText, function(self, text){
+  const node = has2(self, text);
+  node && self.removeChild(node);
   return self;
 });
 
@@ -208,25 +215,18 @@ IEvented.on(_yank.instance, elementText, function(self, text){
 
 const elementElement = signature(isElement, isElement);
 
-IEvented.on(has.instance, elementElement, function(self, child){
+IEvented.on(has2.instance, elementElement, function(self, child){
   return detect(function(node){
     return node === child;
   }, IContent.contents(self));
 });
 
-IEvented.on(_inject.instance, elementElement, function(self, child){
+IEvented.on(inject2.instance, elementElement, function(self, child){
   self.appendChild(child);
   return self;
 });
 
-IEvented.on(_yank.instance, elementElement, function(self, child){
-  self.delChild(child);
+IEvented.on(yank2.instance, elementElement, function(self, child){
+  self.removeChild(child);
   return self;
 });
-
-const inject = reducing(_inject);
-inject.instance = _inject.instance;
-const yank = reducing(_yank);
-yank.instance = _yank.instance;
-
-export {inject, yank};
