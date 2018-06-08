870018b3b16bdd6d861292afe4c8a3fa146fb600
Mario T. Lanza <mlanza@doesideas.com>
2018-06-07 21:03:13 -0400

    make months steppable by dow or day

--- src/types/months/behave.js
+++ src/types/months/behave.js
@@ -1,18 +1,32 @@
-import {ISteppable, IDeref} from '../../protocols';
+import {ISteppable, IConverse, ICloneable} from '../../protocols';
 import {identity, constantly, effect} from '../../core';
 import {implement} from '../protocol';
 import {months} from './construct';
-
-function deref(self){
-  return self.n;
-}
+import {startOfMonth, endOfMonth} from '../date/concrete';
 
 function step(self, dt){
-  var d = new Date(dt.valueOf());
-  d.setMonth(d.getMonth() + self.n);
-  return d;
+  const som  = startOfMonth(dt);
+  const sday = 6 - som.getDay();
+  const calc = ICloneable.clone(som);
+  calc.setMonth(calc.getMonth() + self.n);
+  const eom  = endOfMonth(calc);
+  calc.setDate(self.options.day || dt.getDate());
+  const tgt  = calc > eom ? eom : calc;
+  if (self.options.dow != null) {
+    const tday   = 6 - startOfMonth(tgt).getDay();
+    const offset = tday - sday;
+    tgt.setDate(tgt.getDate() + offset);
+    while(tgt.getDay() != self.options.dow){
+      tgt.setDate(tgt.getDate() - 1);
+    }
+  }
+  return tgt;
+}
+
+function converse(self){
+  return months(self.n * -1, self.options);
 }
 
 export default effect(
-  implement(IDeref, {deref}),
+  implement(IConverse, {converse}),
   implement(ISteppable, {step}));
