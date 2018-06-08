2d2ee1fe6c66119b53a8523373919cc22e5bb207
Mario T. Lanza <mlanza@doesideas.com>
2018-06-07 23:04:07 -0400

    handle eom issues on year steppables

--- src/types/years/behave.js
+++ src/types/years/behave.js
@@ -1,16 +1,26 @@
-import {ISteppable, IDeref} from '../../protocols';
+import {ISteppable, IDeref, ICloneable} from '../../protocols';
 import {identity, constantly, effect} from '../../core';
 import {implement} from '../protocol';
-import {years} from './construct';
+import {startOfMonth, endOfMonth} from '../date/concrete';
+import {min} from '../number/concrete';
 
 function deref(self){
   return self.n;
 }
 
 function step(self, dt){
-  var d = new Date(dt.valueOf());
-  d.setFullYear(d.getFullYear() + self.n);
-  return d;
+  if (dt.getDate() > 28) {
+    const som  = startOfMonth(dt);
+    const calc = ICloneable.clone(som);
+    calc.setFullYear(calc.getFullYear() + self.n);
+    const eom  = endOfMonth(calc);
+    calc.setDate(dt.getDate());
+    return min(calc, eom);
+  } else {
+    const calc = ICloneable.clone(dt);
+    calc.setFullYear(calc.getFullYear() + self.n);
+    return calc;
+  }
 }
 
 export default effect(
