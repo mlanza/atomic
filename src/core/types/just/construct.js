d6b683be20b63025ee465491538809982a58ce4a
Mario T. Lanza <mlanza@doesideas.com>
2018-06-17 20:57:10 -0400

    make constructors accept fs

--- src/core/types/just/construct.js
+++ src/core/types/just/construct.js
@@ -1,3 +1,6 @@
+import {overload} from '../../core';
+import {IReduce, IFunctor} from '../../protocols';
+
 export default function Just(value){
   this.value = value;
 }
@@ -6,6 +9,12 @@ export function just(value){
   return new Just(value);
 }
 
-export function maybe(x){
+function maybe1(x){
   return x == null ? x : just(x);
 }
+
+function maybeN(x, ...fs){
+  return IReduce.reduce(fs, IFunctor.fmap, maybe1(x));
+}
+
+export const maybe = overload(null, maybe1, maybeN);
--- src/core/types/okay/construct.js
+++ src/core/types/okay/construct.js
@@ -1,13 +1,21 @@
+import {overload} from '../../core';
+import {IReduce, IFunctor} from '../../protocols';
 import {isError} from '../error/construct';
 
 export default function Okay(value){
   this.value = value;
 }
 
-export function okay(x){
+function okay1(x){
   return isError(x) ? x : new Okay(x);
 }
 
 export function isOkay(x){
   return x instanceof Okay;
 }
+
+function okayN(x, ...fs){
+  return IReduce.reduce(fs, IFunctor.fmap, okay1(x));
+}
+
+export const okay = overload(null, okay1, okayN);
