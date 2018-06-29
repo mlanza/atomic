afd20b02b9c9bf1c692847ba6a20f2bdd2b0a765
Mario T. Lanza <mlanza@doesideas.com>
2018-06-28 20:28:47 -0400

    add mapSomeKeys

--- src/core/types/object/concrete.js
+++ src/core/types/object/concrete.js
@@ -15,38 +15,51 @@ export function selectKeys(self, keys){
   }, IEmptyableCollection.empty(self));
 }
 
-function mapSomeVals4(self, f, pred, init){
+function mapSomeKeys4(init, self, f, pred){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, key, pred(key) ? f(value) : value);
   }, init);
 }
 
+function mapSomeKeys3(self, f, pred){
+  return mapSomeVals4(IEmptyableCollection.empty(self), self, f, pred);
+}
+
+export const mapSomeKeys = overload(null, null, null, mapSomeKeys3, mapSomeKeys4);
+
+function mapSomeVals4(init, self, f, pred){
+  return IKVReduce.reducekv(self, function(memo, key, value){
+    return IAssociative.assoc(memo, key, pred(value) ? f(value) : value);
+  }, init);
+}
+
 function mapSomeVals3(self, f, pred){
-  return mapSomeVals4(self, f, pred, IEmptyableCollection.empty(self));
+  return mapSomeVals4(IEmptyableCollection.empty(self), self, f, pred);
 }
 
 export const mapSomeVals = overload(null, null, null, mapSomeVals3, mapSomeVals4);
 
-function mapKeys3(self, f, init){
+
+function mapKeys3(init, self, f){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, f(key), value);
   }, IEmptyableCollection.empty(self));
 }
 
 function mapKeys2(self, f){
-  return mapKeys3(self, f, IEmptyableCollection.empty(self));
+  return mapKeys3(IEmptyableCollection.empty(self), self, f);
 }
 
 export const mapKeys = overload(null, null, mapKeys2, mapKeys3);
 
-function mapVals3(self, f, init){
+function mapVals3(init, self, f){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, key, f(value));
   }, init);
 }
 
 function mapVals2(self, f){
-  return mapVals3(self, f, IEmptyableCollection.empty(self));
+  return mapVals3(IEmptyableCollection.empty(self), self, f);
 }
 
 export const mapVals = overload(null, null, mapVals2, mapVals3);
