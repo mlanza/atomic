ed42bddc044bf057a83c780168e3d9980aec3bfe
Mario T. Lanza <me@mlanza.com>
2019-11-26 17:41:26 -0500

    add more hashing

--- src/immutables/index.js
+++ src/immutables/index.js
@@ -1,19 +1,16 @@
-import {doto, implement, toArray, GUID, IEquiv, ICounted, IMap} from "atomic/core";
+import {doto, implement, toArray, reduce, reducekv, str, map, each, get, keys, sort, IEquiv, ICounted, IMap} from "atomic/core";
+import {GUID, AssociativeSubset, Concatenated, EmptyList, Indexed, IndexedSeq} from "atomic/core";
 import {IPersistent, TransientSet} from "atomic/transients";
 import {set} from "./types/set/construct";
 import {IHash} from "./protocols/ihash/instance";
+import {_ as v} from "param.macro";
+
 export * from "./types";
 export * from "./protocols";
 
 export function hashified(Type){
-  if (Type.prototype.hashCode) {
-    throw new Error("Cannot override `hashCode`.");
-  }
   Type.prototype.hashCode = function(){
     return IHash.hash(this);
-  }
-  if (Type.prototype.equals) {
-    throw new Error("Cannot override `equals`.");
   }
   Type.prototype.equals = function(other){
     return IEquiv.equiv(this, other);
@@ -31,13 +28,31 @@ export function hashified(Type){
 
 })();
 
+function combine(h1, h2){
+  return 3 * h1 + h2;
+}
+
 (function(){
 
   function hash(self){
-    return ICounted.count(self); //TODO implement
+    return reduce(combine, 0, map(IHash.hash, self));
   }
 
-  doto(Array,
+  each(
+    doto(v,
+      hashified,
+      implement(IHash, {hash})),
+    [Array, Concatenated, EmptyList]);
+
+})();
+
+(function(){
+
+  function hash(self){
+    return self === true ? 1 : 0;
+  }
+
+  doto(Boolean,
     hashified,
     implement(IHash, {hash}));
 
@@ -46,12 +61,28 @@ export function hashified(Type){
 (function(){
 
   function hash(self){
-    return ICounted.count(IMap.keys(self)); //TODO implement
+    return IHash.hash(str(self));
   }
 
-  doto(Object,
+  doto(Number,
     hashified,
     implement(IHash, {hash}));
+
+})();
+
+(function(){
+
+  function hash(self){
+    return reduce(function(memo, key){
+      return combine(memo, combine(IHash.hash(key), IHash.hash(get(self, key))));
+    }, 0, sort(keys(self)));
+  }
+
+  each(
+    doto(v,
+      hashified,
+      implement(IHash, {hash})),
+    [Object, AssociativeSubset, Indexed, IndexedSeq]);
 
 })();
 
