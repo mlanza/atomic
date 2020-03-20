8a4f4860fea5e3c7002666a921eff3b639781b98
Mario T. Lanza <me@mlanza.com>
2020-03-20 15:06:52 -0400

    make duration mergeable

--- src/core/types/duration/behave.js
+++ src/core/types/duration/behave.js
@@ -1,11 +1,23 @@
 import {implement} from '../protocol';
 import {does, identity} from '../../core';
-import {IFunctor, ISteppable, ICoerceable, IMultipliable, IMap, IAssociative, ILookup, IInclusive} from '../../protocols';
+import {IFunctor, IMergeable, ISteppable, ICoerceable, IMultipliable, IMap, IAssociative, ILookup, IInclusive} from '../../protocols';
 import {reducekv} from '../../protocols/ikvreduce/concrete';
 import {add} from '../../protocols/isteppable/concrete';
 import {isDate} from "../../types/date/concrete";
 import {Duration, duration} from '../duration/construct';
 import {mergeWith} from '../../associatives';
+
+function adds(self, key, value){
+  return IAssociative.assoc(self, key, ILookup.lookup(self, key) + value);
+}
+
+function step(self, dt){
+  return isDate(dt) ? reducekv(adds, dt, self.units) : IMergeable.merge(self, dt);
+}
+
+function merge(self, other){
+  return other ? duration(mergeWith(add, self.units, other.units)) : self;
+}
 
 function mult(self, n){
   return IFunctor.fmap(self, function(value){
@@ -19,39 +31,36 @@ function fmap(self, f){
   }, {}, self.units));
 }
 
-function adds(self, key, value){
-  return IAssociative.assoc(self, key, ILookup.lookup(self, key) + value);
-}
-
-function step(self, dt){
-  return dt == null ? null : isDate(dt) ? reducekv(adds, dt, self.units) : duration(mergeWith(add, dt.units, self.units));
-}
-
 const toDuration = identity;
 
 function keys(self){
   return IMap.keys(self.units);
 }
 
+function dissoc(self, key){
+  return duration(IMap.dissoc(self.units, key));
+}
+
 function lookup(self, key){
   if (!IInclusive.includes(Duration.units, key)){
-    throw new Error("Invalid duration unit.");
+    throw new Error("Invalid unit.");
   }
   return ILookup.lookup(self.units, key);
 }
 
 function assoc(self, key, value){
   if (!IInclusive.includes(Duration.units, key)){
-    throw new Error("Invalid duration unit.");
+    throw new Error("Invalid unit.");
   }
   return duration(IAssociative.assoc(self.units, key, value));
 }
 
 export const behaveAsDuration = does(
+  implement(IMergeable, {merge}),
   implement(IFunctor, {fmap}),
   implement(IAssociative, {assoc}),
   implement(ILookup, {lookup}),
-  implement(IMap, {keys}),
+  implement(IMap, {keys, dissoc}),
   implement(IMultipliable, {mult}),
   implement(ICoerceable, {toDuration}),
   implement(ISteppable, {step}));
