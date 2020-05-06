c2fdf8d761e839fd051845ac201892d17ae4fed7
Mario T. Lanza <mlanza@doesideas.com>
2020-05-06 14:29:35 -0400

    make request associative

--- src/repos/types/request/construct.js
+++ src/repos/types/request/construct.js
@@ -1,13 +1,20 @@
 import * as _ from "atomic/core";
 import {IIntercept} from "../../protocols"
-import {_ as v} from "param.macro";
 
-export function Request(url, filled, options, interceptors, handlers){
+export function Request(url, config, options, interceptors, handlers){
   this.url = url;
-  this.filled = filled;
+  this.config = config;
   this.options = options;
   this.interceptors = interceptors;
   this.handlers = handlers;
+}
+
+export function request(url, context){
+  return new Request(url, context || {}, {}, [fillin], []);
+}
+
+function fillin(self){
+  return new Request(_.fill(self.url, self.config), self.config, filledOptions(self.options, self.config), self.interceptors, self.handlers);
 }
 
 function filledOptions(options, filled){
@@ -16,30 +23,8 @@ function filledOptions(options, filled){
   }, {}, options);
 }
 
-function fillin(self){
-  return new Request(_.fill(self.url, self.filled), self.filled, filledOptions(self.options, self.filled), self.interceptors, self.handlers);
-}
-
-export function mapFills(req, sel, f){
+export function reconfig(req, f){
   return IIntercept.intercept(req, function(self){
-    return new Request(self.url, _.reducekv(function(memo, key, value){
-      return _.assoc(memo, key, sel(key) ? f(value) : value);
-    }, {}, self.filled), self.options, self.interceptors, self.handlers);
+    return new self.constructor(self.url, f(self.config), self.options, self.interceptors, self.handlers);
   }, _.prepend);
-}
-
-const esc = _.pipe(_.replace(v, /&/g, "%26"), _.replace(v, /'/g, "''"));
-
-function escFills2(req, keys){
-  return mapFills(req, _.include(keys, v), esc);
-}
-
-function escFills1(req){
-  return mapFills(req, _.constantly(true), esc);
-}
-
-export const escFills = _.overload(null, escFills1, escFills2);
-
-export function request(url, context){
-  return new Request(url, context || {}, {}, [fillin], []);
 }
