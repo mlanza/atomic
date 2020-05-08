b9514653f203c95b2c911e41f09118b8c9b8631f
Mario T. Lanza <me@mlanza.com>
2020-05-08 16:53:22 -0400

    change fork pipeline

--- src/repos/index.js
+++ src/repos/index.js
@@ -3,6 +3,8 @@ import * as _ from "atomic/core";
 export * from "./types";
 export * from "./protocols";
 export * from "./protocols/concrete";
+import {IParams} from "./protocols";
+import {_ as v} from "param.macro";
 
 export function text(req){
   return _.fmap(req, function(resp){
@@ -24,3 +26,12 @@ export function raise(req){
     });
   })
 }
+
+function params(self, obj){
+  const f = _.isFunction(obj) ? obj : _.merge(v, obj);
+  return _.str(
+    self |> _.split(v, "?") |> _.first,
+    self |> _.fromQueryString |> f |> _.toQueryString);
+}
+
+_.implement(IParams, {params}, String);
