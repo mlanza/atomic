574402f986741a444660053923d8f759802332a9
Mario T. Lanza <me@mlanza.com>
2020-05-13 18:21:42 -0400

    drop emptied

--- src/core/types/object/concrete.js
+++ src/core/types/object/concrete.js
@@ -3,44 +3,42 @@ import {apply, isFunction} from "../function";
 import {reducing} from "../../protocols/ireduce/concrete";
 import {overload, branch} from "../../core";
 import {satisfies} from "../protocol/concrete";
-import {emptyObject} from "./construct";
-
-const emptied = branch(satisfies(IEmptyableCollection), IEmptyableCollection.empty, emptyObject);
+import {empty} from "../../protocols/iemptyablecollection";
 
 export function juxtVals(self, value){
   return IKVReduce.reducekv(self, function(memo, key, f){
     return IAssociative.assoc(memo, key, isFunction(f) ? f(value) : f);
-  }, emptied(self));
+  }, empty(self));
 }
 
 export function selectKeys(self, keys){
   return IReduce.reduce(keys, function(memo, key){
     return IAssociative.assoc(memo, key, ILookup.lookup(self, key));
-  }, emptied(self));
+  }, empty(self));
 }
 
 export function removeKeys(self, keys){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IInclusive.includes(keys, key) ? memo : IAssociative.assoc(memo, key, value);
-  }, emptied(self));
+  }, empty(self));
 }
 
 export function mapKeys(self, f){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, f(key), value);
-  }, emptied(self));
+  }, empty(self));
 }
 
 function mapVals2(self, f){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, key, f(value));
-  }, emptied(self));
+  }, empty(self));
 }
 
 function mapVals3(self, f, pred){
   return IKVReduce.reducekv(self, function(memo, key, value){
     return IAssociative.assoc(memo, key, pred(key) ? f(value) : value);
-  }, emptied(self));
+  }, empty(self));
 }
 
 export const mapVals = overload(null, null, mapVals2, mapVals3);
