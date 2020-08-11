c60f6b7ae3e80d223799aaba3946097ace0ab7ca
Mario T. Lanza <mlanza@doesideas.com>
2020-08-11 16:10:18 -0400

    make left thrushable

--- src/core/types/left/construct.js
+++ src/core/types/left/construct.js
@@ -5,9 +5,11 @@ export function Left(value){
   this.value = value;
 }
 
-export function left(value){
+export function left1(value){
   return new Left(value);
 }
+
+export const left = overload(null, left1, partial(thrush, left1));
 
 export function isLeft(self){
   return self instanceof Left;
