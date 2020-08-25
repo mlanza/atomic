7027fe7fbed6e4be26a4bc9d5a3d3c9635a0d4e3
Mario T. Lanza <mlanza@doesideas.com>
2020-08-25 15:58:09 -0400

    update equality test

--- src/core/protocols/iequiv/concrete.js
+++ src/core/protocols/iequiv/concrete.js
@@ -1,18 +1,21 @@
-import {kin} from "../../core";
+import {overload} from "../../core";
 import {IEquiv} from "./instance";
+import {reduce} from "../ireduce/concrete";
+import {reduced} from "../../types/reduced/construct";
+import {get} from "../ilookup/concrete";
+
 export function equiv(self, other){
   return self === other || IEquiv.equiv(self, other);
 }
 
-export function alike(self, other) { //same parts? structural equality?
-  if (kin(self, other)) {
-    for(let prop of self.getOwnPropertyNames()) {
-      if (!equiv(self[prop], other[prop])) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
+function alike2(self, other){
+  return alike3(self, other, Object.keys(self)); //Object.keys looks to internal properties
 }
+
+function alike3(self, other, keys) { //same parts? structural equality?
+  return reduce(function(memo, key){
+    return memo ? equiv(get(self, key), get(other, key)) : reduced(memo);
+  }, true, keys);
+}
+
+export const alike = overload(null, null, alike2, alike3);
