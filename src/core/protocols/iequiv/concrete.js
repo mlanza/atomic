40cb1d08ce9d2a7bff7d45d0a29fcbae07bcd42e
Mario T. Lanza <mlanza@doesideas.com>
2020-08-27 12:50:43 -0400

    add equivalent

--- src/core/protocols/iequiv/concrete.js
+++ src/core/protocols/iequiv/concrete.js
@@ -1,4 +1,5 @@
-import {overload} from "../../core";
+import {overload, kin} from "../../core";
+import {implement} from "../../types/protocol/concrete";
 import {IEquiv} from "./instance";
 import {reduce} from "../ireduce/concrete";
 import {reduced} from "../../types/reduced/construct";
@@ -14,8 +15,15 @@ function alike2(self, other){
 
 function alike3(self, other, keys) { //same parts? structural equality?
   return reduce(function(memo, key){
-    return memo ? equiv(get(self, key), get(other, key)) : reduced(memo);
+    return memo ? equiv(self[key], other[key]) : reduced(memo);
   }, true, keys);
 }
 
 export const alike = overload(null, null, alike2, alike3);
+
+export function equivalent(){
+  function equiv(self, other){
+    return kin(self, other) && alike(self, other);
+  }
+  return implement(IEquiv, {equiv});
+}
