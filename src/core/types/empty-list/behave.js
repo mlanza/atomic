5e04a496cdda7b7eeb6a8ad3b30365c0bc8424c7
Mario T. Lanza <me@mlanza.com>
2020-09-10 19:44:20 -0400

    fix equiv issue

--- src/core/types/empty-list/behave.js
+++ src/core/types/empty-list/behave.js
@@ -1,5 +1,5 @@
 import {identity, constantly} from '../../core';
-import {implement, packs as does} from '../protocol';
+import {satisfies, implement, packs as does} from '../protocol';
 import {IEquiv, IBlankable, ICoerceable, IInclusive, IReversible, ICollection, INext, ISeq, ISeqable, ISequential, IAssociative, IIndexed, IEmptyableCollection, IKVReduce, IReduce, ICounted} from '../../protocols';
 import {emptyList, EmptyList} from '../../types/empty-list/construct';
 import {emptyArray} from '../../types/array/construct';
@@ -10,6 +10,9 @@ function reduce(self, f, init){
 }
 
 function equiv(as, bs){
+  if (!satisfies(ISeqable, bs)) {
+    return false;
+  }
   const xs = ISeqable.seq(as),
         ys = ISeqable.seq(bs);
   return ICounted.count(xs) === ICounted.count(ys) && IEquiv.equiv(ISeq.first(xs), ISeq.first(ys)) && IEquiv.equiv(INext.next(xs), INext.next(ys));
