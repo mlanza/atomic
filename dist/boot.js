c3eadeddc6ea9456fdcded4d1b1ff340d9355e3c
Mario T. Lanza <me@mlanza.com>
2021-06-02 18:08:45 -0400

    split dev

--- dist/boot.js
+++ dist/boot.js
@@ -35,9 +35,6 @@ define('weak-map', window.WeakMap ? [] : ["/dist/collections-es6.js"], function(
 define('weak-set', window.WeakSet ? [] : ["/dist/collections-es6.js"], function(){
   return window.WeakSet;
 });
-define('context', function(){
-  return {};
-});
 define('dom', [], function(){
   return {
     document: document,
@@ -48,47 +45,31 @@ define('dom', [], function(){
     HTMLSelectElement: HTMLSelectElement,
     NodeList: NodeList,
     Comment: Comment,
-    HTMLDocument: window.HTMLDocument || window.Document,
-    XMLDocument: window.XMLDocument || window.Document
+    HTMLDocument: window.HTMLDocument || window.Document, //IE compatible
+    XMLDocument: window.XMLDocument || window.Document //IE compatible
   }
 });
-define('atomic/core', ["/dist/atomic/core.js"], function(core){
-  return Object.assign(core.placeholder, core.impart(core, core.partly));
-});
-define('atomic/dom', ["atomic/core", "/dist/atomic/dom.js"], function(core, dom){
-  return core.impart(dom, core.partly);
-});
-define('atomic/immutables', ["atomic/core", "/dist/atomic/immutables.js"], function(core, immutables){
-  return core.impart(immutables, core.partly);
-});
-define('atomic/reactives', ["atomic/core", "/dist/atomic/reactives.js"], function(core, reactives){
-  return core.impart(reactives, core.partly);
-});
-define('atomic/repos', ["atomic/core", "/dist/atomic/repos.js"], function(core, repos){
-  return core.impart(repos, core.partly);
-});
-define('atomic/transducers', ["atomic/core", "/dist/atomic/transducers.js"], function(core, transducers){
-  return core.impart(transducers, core.partly);
-});
-define('atomic/transients', ["atomic/core", "/dist/atomic/transients.js"], function(core, transients){
-  return core.impart(transients, core.partly);
-});
-define('atomic/validates', ["atomic/core", "/dist/atomic/validates.js"], function(core, validates){
-  return core.impart(validates, core.partly);
-});
-define('atomic/draw', ["atomic/core", "/dist/atomic/draw.js"], function(core, draw){
-  return core.impart(draw, core.partly);
-});
-define('atomic', ['atomic/core', 'atomic/dom', 'atomic/immutables', 'atomic/reactives', 'atomic/transducers', 'atomic/transients', 'atomic/repos', 'atomic/validates', 'atomic/draw'], function(core, dom, immutables, reactives, transducers, transients, repos, validates, draw){
-  return Object.assign(core, { //for in-browser testing, not for production use.
-    core: core,
-    dom: dom,
-    immutables: immutables,
-    reactives: reactives,
-    transducers: transducers,
-    transients: transients,
-    validates: validates,
-    draw: draw,
-    repos: repos
+(function(mods){
+  define('atomic/core', [`/dist/${mods[0]}.js`], function(core){
+    return Object.assign(core.placeholder, core.impart(core, core.partly));
   });
-});
+  for (var i = 1; i < mods.length; i++) {
+    var mod = mods[i];
+    define(mod, ["atomic/core", `/dist/${mod}.js`], function(core, tgt){
+      return core.impart(tgt, core.partly);
+    });
+  }
+  define('atomic', mods, function(core, dom, immutables, reactives, repos, transducers, transients, validates, draw){
+    return { //exposed purely for in-browser repl use.
+      core: core,
+      dom: dom,
+      immutables: immutables,
+      reactives: reactives,
+      transducers: transducers,
+      transients: transients,
+      validates: validates,
+      draw: draw,
+      repos: repos
+    };
+  });
+})(["atomic/core", "atomic/dom", "atomic/immutables", "atomic/reactives", "atomic/repos", "atomic/transducers", "atomic/transients", "atomic/validates", "atomic/draw"]);
