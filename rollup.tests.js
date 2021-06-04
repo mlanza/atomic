4376213ad9787020aa1d29346991232a8698978b
Mario T. Lanza <mlanza@doesideas.com>
2021-06-04 19:50:45 -0400

    move scripts/styles to assets

--- rollup.tests.js
+++ rollup.tests.js
@@ -5,7 +5,7 @@ import json  from '@rollup/plugin-json';
 export default {
   input: ['src/tests.js'],
   output: {
-    dir: 'public/scripts',
+    dir: 'public/assets',
     format: 'amd',
     interop: false,
     globals: {
