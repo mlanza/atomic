41a7b33865b1f72e20d4d918b73c022acc6ce326
Mario T. Lanza <me@mlanza.com>
2021-06-13 22:53:13 -0400

    pipe passes along err & complete

--- src/reactives/types/observer/construct.js
+++ src/reactives/types/observer/construct.js
@@ -1,4 +1,5 @@
 import {noop} from "atomic/core";
+import {IPublish} from "../../protocols/ipublish.js";
 
 export function Observer(pub, err, complete, terminated){
   this.pub = pub;
@@ -11,3 +12,8 @@ export function observer(pub, err, complete){
   return new Observer(pub || noop, err || noop, complete || noop, null);
 }
 
+export function observe(pub, obs){
+  return observer(pub, IPublish.err(obs, ?), function(){
+    IPublish.complete(obs);
+  });
+}
