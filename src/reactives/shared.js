4911260d05a16c9b30c18e131c3082335adced44
Mario T. Lanza <me@mlanza.com>
2021-06-23 12:17:50 -0400

    share reactive mergeability

--- src/reactives/shared.js
+++ src/reactives/shared.js
@@ -1,8 +1,18 @@
-import {implement, IReduce} from "atomic/core";
+import {implement, does, IMergeable, IReduce} from "atomic/core";
+import {observable} from "./types/observable/construct.js"
 import {ISubscribe} from "./protocols/isubscribe/instance.js";
+import {IPublish} from "./protocols/ipublish/instance.js";
+
+function merge(self, other){
+  return observable(function(observer){
+    const handle = IPublish.pub(observer, ?);
+    return does(ISubscribe.sub(self, handle), ISubscribe.sub(other, handle));
+  });
+}
 
 function reduce(self, xf, init){
   return ISubscribe.sub(init, xf(self, ?));
 }
 
+export const imergeable = implement(IMergeable, {merge});
 export const ireduce = implement(IReduce, {reduce});
