90e9eb7fdd4e120f2faa731c4be28b6a7f1d0a1c
Mario T. Lanza <me@mlanza.com>
2021-06-24 23:15:13 -0400

    add arity 0

--- src/core/protocols/icollection/concrete.js
+++ src/core/protocols/icollection/concrete.js
@@ -1,5 +1,7 @@
 import {ICollection} from "./instance.js";
 import {overload, identity} from "../../core.js";
 import {reducing} from "../ireduce/concrete.js";
-export const conj = overload(null, identity, ICollection.conj, reducing(ICollection.conj));
+export const conj = overload(function(){
+  return [];
+}, identity, ICollection.conj, reducing(ICollection.conj));
 export const unconj = overload(null, identity, ICollection.unconj, reducing(ICollection.unconj));
