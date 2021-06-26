6d316688b61d1b813035720af01f883aec8105ae
Mario T. Lanza <me@mlanza.com>
2021-06-25 20:46:14 -0400

    add forward

--- src/transients/types/transient-array/behave.js
+++ src/transients/types/transient-array/behave.js
@@ -1,4 +1,4 @@
-import {does, overload, doto, forwardTo, implement} from "atomic/core";
+import {does, overload, doto, forward, implement} from "atomic/core";
 import {ICoerceable, IFunctor, ILookup, IAssociative, IFind, IMapEntry, IYankable, ISeq, INext, ISeqable, ICounted, IInclusive, IEmptyableCollection, IMap, IReduce, IKVReduce, ICloneable, ISequential, ICollection} from "atomic/core";
 import {IPersistent, ITransientMap, ITransientInsertable, ITransientEmptyableCollection, ITransientReversible, ITransientYankable, ITransientAssociative, ITransientAppendable, ITransientPrependable, ITransientCollection} from "../../protocols.js";
 
@@ -64,47 +64,18 @@ function persistent(self){
   return arr;
 }
 
-const forward = forwardTo("arr");
-const find = forward(IFind.find);
-const key = forward(IMapEntry.key);
-const val = forward(IMapEntry.val);
-const contains = forward(IAssociative.contains);
-const keys = forward(IMap.keys);
-const vals = forward(IMap.vals);
-const toObject = forward(ICoerceable.toObject);
-const lookup = forward(ILookup.lookup);
-const reduce = forward(IReduce.reduce);
-const reducekv = forward(IKVReduce.reducekv);
-const fmap = forward(IFunctor.fmap);
-const includes = forward(IInclusive.includes);
-const count = forward(ICounted.count);
-const first = forward(ISeq.first);
-const rest = forward(ISeq.rest);
-const next = forward(INext.next);
-
 export const behaveAsTransientArray = does(
+  forward("arr", IFind, IMapEntry, IAssociative, IMap, ICoerceable, ILookup, IReduce, IKVReduce, IFunctor, IInclusive, ICounted, ISeq, INext),
   implement(ISequential),
   implement(ICloneable, {clone}),
   implement(IPersistent, {persistent}),
   implement(ISeqable, {seq}),
-  implement(ISeq, {first, rest}),
-  implement(INext, {next}),
-  implement(ICounted, {count}),
   implement(ITransientInsertable, {before, after}),
   implement(ITransientCollection, {conj: append, unconj}),
   implement(ITransientEmptyableCollection, {empty}),
-  implement(IFind, {find}),
   implement(ITransientYankable, {yank}),
-  implement(IMapEntry, {key, val}),
-  implement(ILookup, {lookup}),
-  implement(IAssociative, {contains}),
   implement(ITransientAssociative, {assoc}),
   implement(ITransientReversible, {reverse}),
   implement(ITransientMap, {dissoc}),
-  implement(IMap, {keys, vals}),
-  implement(ICoerceable, {toObject}),
-  implement(IReduce, {reduce}),
-  implement(IKVReduce, {reducekv}),
   implement(ITransientAppendable, {append}),
-  implement(ITransientPrependable, {prepend}),
-  implement(IFunctor, {fmap}));
+  implement(ITransientPrependable, {prepend}));
