1953e7e05fdaa5e87440d12f42d0974632bfb896
Mario T. Lanza <mlanza@doesideas.com>
2021-07-02 14:55:56 -0400

    pass document default

--- src/dom/types/element/construct.js
+++ src/dom/types/element/construct.js
@@ -1,19 +1,19 @@
-import {each, partial, flatten, curry, pre, apply, signatureHead, isString} from "atomic/core";
+import {each, partial, curry, handle, flatten, apply} from "atomic/core";
 import {Element} from "dom";
 import {embed, embeds} from "../../protocols/iembeddable.js";
-import {isHTMLDocument} from "../html-document/construct.js";
+import {passDocumentDefault} from "../html-document/construct.js";
 
-export const element = curry(pre(function element(document, name, ...contents){
+export const element = curry(passDocumentDefault(function element(document, name, ...contents){
   const el = document.createElement(name);
   apply(embeds, el, flatten(contents));
   return el;
-}, signatureHead(isHTMLDocument, isString)), 2);
+}), 2);
 
-export const elementns = curry(pre(function elementns(document, ns, name, ...contents){
+export const elementns = curry(passDocumentDefault(function elementns(document, ns, name, ...contents){
   const el = document.createElementNS(ns, name);
   apply(embeds, el, flatten(contents));
   return el;
-}, signatureHead(isHTMLDocument, isString, isString)), 3);
+}), 3);
 
 export function isElement(self){
   return self instanceof Element;
