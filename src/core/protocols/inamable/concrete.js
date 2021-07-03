3e412aeb25f73571140548f8ec65240a2c1d45e2
Mario T. Lanza <me@mlanza.com>
2021-07-03 09:38:44 -0400

    refine tags

--- src/core/protocols/inamable/concrete.js
+++ src/core/protocols/inamable/concrete.js
@@ -1,20 +1,24 @@
-import {type, doto, constantly, overload, pre} from "../../core.js";
+import {type, doto, constantly, overload, pre, post} from "../../core.js";
 import {INamable} from "./instance.js";
 import {specify} from "../../types/protocol/concrete.js";
 import Symbol from "symbol";
 
 export const name = INamable.name;
 
-//tool for overcoming cross-frame type checking
-// see: https://github.com/mrdoob/three.js/issues/5886
-// see: http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
-export function what(self){
-  return self == null ? null : name(type(self));
+export function isSymbol(self){
+  return typeof self === "symbol";
 }
 
 function hasSymbolicName(f, symbol){
   return typeof f === "function" && typeof symbol === "symbol";
 }
+
+//tool for overcoming cross-frame type checking
+// see: https://github.com/mrdoob/three.js/issues/5886
+// see: http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
+export const what = post(function what(self){
+  return self == null ? null : name(type(self));
+}, isSymbol);
 
 export const naming = pre(function naming(type, symbol){
   if (typeof name(type) !== "symbol") {
