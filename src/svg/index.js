f77b2d8f807a4feec18e42f5d2e842ab7a162039
Mario T. Lanza <mlanza@doesideas.com>
2021-07-03 10:08:21 -0400

    allow other svg contexts

--- src/svg/index.js
+++ src/svg/index.js
@@ -1,13 +1,27 @@
-import {factory} from "atomic/core";
-import {elementns, embeds, tags} from "atomic/dom";
+import {overload} from "atomic/core";
+import * as dom from "atomic/dom";
 import {document} from "dom";
 
-export const ns = elementns(document, "http://www.w3.org/2000/svg");
-export const svgTags = ["svg", "g", "symbol", "defs", "clipPath", "metadata", "path", "line", "circle", "rect", "ellipse", "polygon", "polyline", "image", "text", "tspan"];
-export const {svg, g, symbol, defs, clipPath, metadata, path, line, circle, rect, ellipse, polygon, polyline, image, text, tspan} = tags(ns, svgTags);
-export function use(link, ...contents){
-  const el = ns("use");
-  el.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', link);
-  embeds(el, ...contents);
-  return el;
+export const taglist = ["svg", "g", "symbol", "defs", "clipPath", "metadata", "path", "line", "circle", "rect", "ellipse", "polygon", "polyline", "image", "text", "tspan"];
+
+function tags2(document, list){
+  const ns = dom.elementns(document, "http://www.w3.org/2000/svg");
+  const tags = dom.tags(ns, list);
+
+  function use(link, ...contents){
+    const el = ns("use");
+    el.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', link);
+    dom.embeds(el, ...contents);
+    return el;
+  }
+
+  tags["use"] = use;
+  return tags;
 }
+
+function tags1(document){
+  return tags2(document, taglist);
+}
+
+export const tags = overload(null, tags1, tags2);
+export const {svg, g, symbol, defs, clipPath, metadata, path, line, circle, rect, ellipse, polygon, polyline, image, text, tspan, use} = tags(document);
