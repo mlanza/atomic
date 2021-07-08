8689f4a5621703bb542df7404c62703ae790b073
Mario T. Lanza <me@mlanza.com>
2021-07-08 11:35:30 -0400

    multimethod is handler

--- src/transients/types/multimethod/construct.js
+++ src/transients/types/multimethod/construct.js
@@ -1,5 +1,6 @@
 import * as _ from "atomic/core";
 import {ITransientCollection} from "../../protocols.js";
+import {method} from "../method/construct.js";
 
 function surrogate(f, substitute){
   return function(self, ...args){
@@ -14,7 +15,7 @@ export function Multimethod(methods, fallback){
 }
 
 export function multimethod(fallback){
-  const instance = new Multimethod([], fallback),
+  const instance = new Multimethod([], fallback ? method(_.constantly(true), fallback) : null),
         fn = _.partial(_.invoke, instance),
         conj = surrogate(ITransientCollection.conj, instance);
   return _.doto(fn,
