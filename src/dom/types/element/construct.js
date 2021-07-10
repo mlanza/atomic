73ba65d072ee53cfe25783f592576c41a4da5b22
Mario T. Lanza <mlanza@doesideas.com>
2021-07-10 11:13:47 -0400

    conjable select

--- src/dom/types/element/construct.js
+++ src/dom/types/element/construct.js
@@ -4,15 +4,13 @@ import {embed, embeds} from "../../protocols/iembeddable.js";
 import {isHTMLDocument} from "../html-document/construct.js";
 
 export const element = _.assume(isHTMLDocument, document, _.curry(function element(document, name, ...contents){
-  const el = document.createElement(name);
-  _.apply(embeds, el, _.flatten(contents));
-  return el;
+  return _.doto(document.createElement(name),
+    _.apply(embeds, ?, _.flatten(contents)));
 }, 2));
 
 export const elementns = _.assume(isHTMLDocument, document, _.curry(function elementns(document, ns, name, ...contents){
-  const el = document.createElementNS(ns, name);
-  _.apply(embeds, el, _.flatten(contents));
-  return el;
+  return _.doto(document.createElementNS(ns, name),
+    _.apply(embeds, ?, _.flatten(contents)));
 }, 3));
 
 export function isElement(self){
