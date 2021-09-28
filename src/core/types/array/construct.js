80851e7a6a47790ae51c50b3e65229e4abbaa8ea
Mario T. Lanza <me@mlanza.com>
2021-09-27 22:27:02 -0400

    fix array

--- src/core/types/array/construct.js
+++ src/core/types/array/construct.js
@@ -1,6 +1,8 @@
 import {constructs} from "../../core.js";
 
-export const array = constructs(Array);
+export function array(...args){
+  return args;
+}
 
 export function emptyArray(){
   return [];
