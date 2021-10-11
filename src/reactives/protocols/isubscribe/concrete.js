039c33cf8486ff51fadf99945f5bfa235cb211c7
Mario T. Lanza <me@mlanza.com>
2021-10-11 11:12:37 -0400

    use transducing

--- src/reactives/protocols/isubscribe/concrete.js
+++ src/reactives/protocols/isubscribe/concrete.js
@@ -3,13 +3,13 @@ import {pub} from "../../protocols/ipublish/concrete.js";
 import {ISubscribe} from "./instance.js";
 
 function sub3(source, xf, sink){
-  return ISubscribe.transducing(source, xf, sink); //TODO import transducing logic directly
+  return ISubscribe.transducing(source, xf, sink);
 }
 
 function subN(source){
   const sink = arguments[arguments.length - 1],
         xfs = _.slice(arguments, 1, arguments.length - 1);
-  return sub3(source, _.comp(...xfs), sink);
+  return ISubscribe.transducing(source, _.comp(...xfs), sink);
 }
 
 export const sub = _.overload(null, null, ISubscribe.sub, sub3, subN);
