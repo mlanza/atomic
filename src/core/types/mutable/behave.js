40ffeb4c41934d13e2568c2cc940d5b61fe8d310
Mario T. Lanza <mlanza@doesideas.com>
2021-10-11 12:07:38 -0400

    add fmap to mutable

--- src/core/types/mutable/behave.js
+++ src/core/types/mutable/behave.js
@@ -1,6 +1,6 @@
 import {does} from "../../core.js";
 import {implement} from "../protocol.js";
-import {IDeref, ISwap, IReset} from "../../protocols.js";
+import {IDeref, ISwap, IReset, IFunctor} from "../../protocols.js";
 import {naming} from "../../protocols/inamable/concrete.js";
 
 function deref(self){
@@ -15,8 +15,14 @@ function reset(self, obj){
   return self.obj = obj;
 }
 
+function fmap(self, f){
+  swap(self, f);
+  return self;
+}
+
 export default does(
   naming("Mutable"),
+  implement(IFunctor, {fmap}),
   implement(IReset, {reset}),
   implement(ISwap, {swap}),
   implement(IDeref, {deref}));
