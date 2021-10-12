1b14436a800283d6183a725bca3153eeb8d6c574
Mario T. Lanza <mlanza@doesideas.com>
2021-10-12 07:06:41 -0400

    tweak loggers

--- src/core/types/function/protocols.js
+++ src/core/types/function/protocols.js
@@ -2,4 +2,3 @@ export * from "../../protocols/icoercible/concrete.js";
 export * from "../../protocols/ireduce/concrete.js";
 export * from "../../protocols/icollection/concrete.js";
 export * from "../../protocols/ilookup/concrete.js";
-export * from "../../protocols/ilogger/concrete.js";
