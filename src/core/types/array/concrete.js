c45c811c57fb14aa6b90373920ffdc5be06b35b7
Mario T. Lanza <mlanza@doesideas.com>
2021-10-18 14:39:00 -0400

    use safer toArray approach

--- src/core/types/array/concrete.js
+++ src/core/types/array/concrete.js
@@ -1,12 +1,8 @@
 import {is} from "../../protocols/imapentry/concrete.js";
-import {satisfies} from "../protocol.js";
-import {ICoercible} from "../../protocols/icoercible/instance.js";
+import {coerce} from "../../protocols/icoercible/concrete.js";
 
 export function isArray(self){
   return is(self, Array);
 }
 
-export function toArray(self){
-  const f = satisfies(ICoercible, "coerce", self);
-  return f ? f(self, Array) : Array.from(self);
-}
+export const toArray = coerce(?, Array);
