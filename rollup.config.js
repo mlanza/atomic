7a6974447a7e0aabfc459ed8570810feadcbe3f8
Mario T. Lanza <me@mlanza.com>
2021-10-18 22:31:40 -0400

    single bundler

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,44 @@ import jscc from 'rollup-plugin-jscc';
 const _CROSSFRAME = process.argv.indexOf("--crossframe") == -1 ? 0 : 1;
 console.log("crossframe", _CROSSFRAME);
 
-export default {
+const external = [
+  "immutable",
+  "hash",
+  "promise",
+  "fetch",
+  "symbol",
+  "weak-map",
+  "set",
+  "map",
+  "qunit",
+  "jquery",
+  "dom",
+  "atomic",
+  "atomic/core",
+  "atomic/immutables",
+  "atomic/shell",
+  "atomic/reactives",
+  "atomic/transducers",
+  "atomic/transients",
+  "atomic/repos",
+  "atomic/validates",
+  "atomic/html",
+  "atomic/svg",
+  "atomic/dom"
+];
+
+const globals = {
+  "jquery": "jQuery",
+  "qunit": "QUnit",
+  "fetch": "fetch",
+  "map": "Map",
+  "set": "Set",
+  "weak-map": "WeakMap",
+  "symbol": "Symbol",
+  "promise": "Promise"
+};
+
+export default [{
   input: [
     'src/core.js',
     'src/dom.js',
@@ -24,42 +61,9 @@ export default {
     dir: 'public/assets/vendor/atomic',
     format: 'amd',
     interop: false,
-    globals: {
-      "jquery": "jQuery",
-      "qunit": "QUnit",
-      "fetch": "fetch",
-      "map": "Map",
-      "set": "Set",
-      "weak-map": "WeakMap",
-      "symbol": "Symbol",
-      "promise": "Promise"
-    }
+    globals
   },
-  external: [
-    "immutable",
-    "hash",
-    "promise",
-    "fetch",
-    "symbol",
-    "weak-map",
-    "set",
-    "map",
-    "qunit",
-    "jquery",
-    "dom",
-    "atomic",
-    "atomic/core",
-    "atomic/immutables",
-    "atomic/shell",
-    "atomic/reactives",
-    "atomic/transducers",
-    "atomic/transients",
-    "atomic/repos",
-    "atomic/validates",
-    "atomic/html",
-    "atomic/svg",
-    "atomic/dom"
-  ],
+  external,
   plugins: [
     resolve(),
     jscc({
@@ -71,4 +75,21 @@ export default {
     }),
     json()
   ]
-};
+},{
+  input: ['src/tests.js'],
+  output: {
+    dir: 'public/assets',
+    format: 'amd',
+    interop: false,
+    globals
+  },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
+    }),
+    json()
+  ],
+  external
+}];
