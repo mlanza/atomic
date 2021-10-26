26e6ad7416db784fd5b837a9dd46209d1c62db22
Mario T. Lanza <me@mlanza.com>
2021-10-26 17:22:40 -0400

    drop obs journal

--- src/reactives/types.js
+++ src/reactives/types.js
@@ -1,6 +1,5 @@
 export * from "./types/cell.js";
 export * from "./types/cursor.js";
-export * from "./types/journal.js";
 export * from "./types/observable.js";
 export * from "./types/observer.js";
 export * from "./types/subject.js";
--- src/reactives/types/journal.js
+++
@@ -1,4 +0,0 @@
-export * from "./journal/construct.js";
-import {Journal} from "./journal/construct.js";
-import behave from "./journal/behave.js";
-behave(Journal);
