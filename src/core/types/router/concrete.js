92e4e280bdd4da0a99516fd99c7ba9e08d0368ae
Mario T. Lanza <mlanza@doesideas.com>
2021-11-02 14:26:29 -0400

    make router persistent

--- src/core/types/router/concrete.js
+++ src/core/types/router/concrete.js
@@ -5,18 +5,22 @@ import {append} from "../../protocols/iappendable/concrete.js";
 import {concat} from "../../types/concatenated/construct.js";
 import {apply} from "../../types/function/concrete.js";
 import {reGroups} from "../../types/reg-exp/concrete.js";
+import {Router} from "./construct.js";
 
 function addRoute3(self, pred, f){
-  addRoute2(self, guard(pred, f));
+  return addRoute2(self, guard(pred, f));
 }
 
 function addRoute2(self, handler){
-  self.handlers = append(self.handlers, handler);
-  self.f = apply(coalesce, concat(self.handlers, [self.fallback]));
+  const handlers = append(self.handlers, handler);
+  return new Router(
+    handlers,
+    self.fallback,
+    apply(coalesce, concat(handlers, [self.fallback])));
 }
 
 function addRoute4(self, re, xf, f){
-  addRoute2(self, parsedo(reGroups(re, ?), xf, f));
+  return addRoute2(self, parsedo(reGroups(re, ?), xf, f));
 }
 
 export const addRoute = overload(null, null, addRoute2, addRoute3, addRoute4);
