6ef233117682dbdc7c7b1e161faf7f2314f9af22
Mario T. Lanza <mlanza@doesideas.com>
2021-11-02 18:39:20 -0400

    implement assoc

--- src/core/types/associative-subset/protocols.js
+++ src/core/types/associative-subset/protocols.js
@@ -1,6 +1,7 @@
 export * from "../../protocols/iinclusive/concrete.js";
 export * from "../../protocols/icounted/concrete.js";
 export * from "../../protocols/ilookup/concrete.js";
+export * from "../../protocols/iassociative/concrete.js";
 export * from "../../protocols/iseq/concrete.js";
 export * from "../../protocols/iseqable/concrete.js";
 export * from "../../protocols/imap/concrete.js";
