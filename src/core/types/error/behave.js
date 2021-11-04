fbb7475ca39005dc101db698a1f5acb8fd11f6d0
Mario T. Lanza <me@mlanza.com>
2021-11-04 18:57:06 -0400

    error not functor

--- src/core/types/error/behave.js
+++ src/core/types/error/behave.js
@@ -1,12 +1,3 @@
-import {IFunctor, IForkable} from "../../protocols.js";
-import {identity, does} from "../../core.js";
-import {implement} from "../protocol.js";
 import {keying} from "../../protocols/imapentry/concrete.js";
 
-function fork(self, reject, resolve){
-  reject(self);
-}
-
-export default does(
-  keying("Error"),
-  implement(IForkable, {fork}));
+export default keying("Error");
