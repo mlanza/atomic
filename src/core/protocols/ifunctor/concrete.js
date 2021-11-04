f8867c93420f6914da76e71c5373534fa4f33831
Mario T. Lanza <mlanza@doesideas.com>
2021-11-04 18:40:18 -0400

    standardize thrush

--- src/core/protocols/ifunctor/concrete.js
+++ src/core/protocols/ifunctor/concrete.js
@@ -5,9 +5,15 @@ import {IFunctor} from "./instance.js";
 
 export const fmap = overload(constantly(identity), IFunctor.fmap, reducing(IFunctor.fmap));
 
-export function thrush(unit, init, ...fs){
+function thrushN(unit, init, ...fs){
   return deref(reduce(IFunctor.fmap, unit(init), fs));
 }
+
+function thrush1(f){
+  return overload(null, f, partial(thrushN, f));
+}
+
+export const thrush = overload(null, thrush1, thrushN);
 
 function pipeline1(unit){
   return partial(pipelineN, unit);
