b819a4a9a9b60d27a273082cb10abb8279ddbfb5
Mario T. Lanza <me@mlanza.com>
2021-11-06 10:42:10 -0400

    replace fluent with verified

--- src/core/types/verified/behave.js
+++ src/core/types/verified/behave.js
@@ -1,12 +1,12 @@
 import {implement} from "../protocol.js";
 import {does} from "../../core.js";
-import {fluent} from "./construct.js";
+import {Verified} from "./construct.js";
 import {IFunctor, IDeref} from "../../protocols.js";
 import {keying} from "../../protocols/imapentry/concrete.js";
 
 function fmap(self, f){
   const value = f(self.value);
-  return fluent(value == undefined ? self.value : value);
+  return new Verified(self.pred(value) ? value : self.value, self.pred);
 }
 
 function deref(self){
@@ -14,6 +14,6 @@ function deref(self){
 }
 
 export default does(
-  keying("Fluent"),
+  keying("Verified"),
   implement(IDeref, {deref}),
   implement(IFunctor, {fmap}));
