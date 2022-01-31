bb25b74385cbfb1f5b2e72dd53fde1180ea683da
Mario T. Lanza <me@mlanza.com>
2022-01-31 08:20:16 -0500

    adjust config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
 import json  from '@rollup/plugin-json';
 import jscc from 'rollup-plugin-jscc';
 
