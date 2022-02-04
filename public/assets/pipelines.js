e556a567e7bfce17604971ba231687bad50f27c8
Mario T. Lanza <me@mlanza.com>
2022-02-04 18:01:39 -0500

    move pipelines

--- public/assets/pipelines.js
+++ public/assets/pipelines.js
@@ -1,5 +1,5 @@
-import {log, lowerCase, map, mapa, just, join, pipe, str, range} from "./public/assets/vendor/atomic/core.js";
-import * as core from "./public/assets/vendor/atomic/core.js";
+import {log, lowerCase, map, mapa, thread, join, pipe, str, range} from "./vendor/atomic/core.js";
+import _ from "./vendor/@atomic/core.js";
 
 /* WANTED: point-free style, esp. with pipelined operations (e.g. threading macro) */
 
@@ -10,13 +10,13 @@ import * as core from "./public/assets/vendor/atomic/core.js";
 //  |> log;
 
 //Arrow functions help, but are noisy and not point-free.
-just(range(10),
-  x => map(pipe(y => str("Count ", y), lowerCase), x),
+thread(range(10),
+  x => map(pipe(y => str("Number ", y), lowerCase), x),
   x => join(", ", x),
   log);
 
 //See how `boot.js` imbues typical functions with partial application so the following just works.
-_.just(_.range(10),
+_.thread(_.range(10),
   _.map(_.pipe(_.str("Number ", _), _.lowerCase), _),
   _.join(", ", _),
   _.log);
