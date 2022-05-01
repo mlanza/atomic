944bbe0521327d7008f643e10f61af430fe56519
Mario T. Lanza <me@mlanza.com>
2022-04-30 21:05:34 -0400

    fix unique

--- src/core/protocols/icoercible/instance.js
+++ src/core/protocols/icoercible/instance.js
@@ -1,9 +1,17 @@
-import {protocol} from "../../types/protocol.js";
+import {protocol, satisfies} from "../../types/protocol.js";
 import {invoke} from "../../protocols/ifn/concrete.js";
-import {key} from "../../protocols/imapentry/concrete.js";
+import {key as k} from "../../protocols/imapentry/concrete.js";
 import {type} from "../../core.js";
 import {multimethod} from "../../types/multimethod/construct.js";
 import {addMethod as _addMethod} from "../../types/multimethod/concrete.js";
+
+function key(self){
+  try {
+    return k(self);
+  } catch {
+    return self; //coercible fallback
+  }
+}
 
 //hide the details of using `key` for potential cross-frame compatibility
 const mm = multimethod(function(source, Type){
