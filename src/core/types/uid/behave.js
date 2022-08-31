aeb95c07dd066268aa7c4a52b26a45c747767a80
Mario T. Lanza <me@mlanza.com>
2022-08-31 09:34:10 -0400

    uid is contextually unique

--- src/core/types/uid/behave.js
+++ src/core/types/uid/behave.js
@@ -1,7 +1,18 @@
 import {does} from "../../core.js";
-import behave from "../guid/behave.js";
+import {implement} from "../protocol.js";
+import * as p from "../../protocols.js";
+import * as P from "../../protocols/concrete.js";
 import {keying} from "../../protocols/imapentry/concrete.js";
 
+function equiv(self, other){
+  return P.equiv(self.id, other.id) && P.equiv(self.context, other.context);
+}
+
+function hash(self){
+  return P.hash(self.id + "/" + (self.context || ""));
+}
+
 export default does(
-  behave,
+  implement(p.IEquiv, {equiv}),
+  implement(p.IHashable, {hash}),
   keying("UID"));
