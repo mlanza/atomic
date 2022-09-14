50f7fa67b0bfd04182fbbe6c7c6fd85e2b534bd1
Mario T. Lanza <mlanza@doesideas.com>
2022-09-13 20:54:47 -0400

    fix bundling issue for plv8 deployment

--- src/core/types/multimethod/concrete.js
+++ src/core/types/multimethod/concrete.js
@@ -2,7 +2,7 @@ import {hash} from "../../protocols/ihashable/concrete.js";
 
 export function addMethod(self, key, handler){
   const hashcode = hash(key);
-  const potentials = self.methods[hashcode] ||= [];
+  const potentials = (self.methods[hashcode] = self.methods[hashcode] || []);
   potentials.push([key, handler]);
   return self;
 }
