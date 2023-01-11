652255537f5d4ed3557a04daaf43f0cedb0a7dc4
Mario T. Lanza <me@mlanza.com>
2023-01-10 20:00:24 -0500

    update readme
    update readme

--- tests/autopartial-less.js
+++ tests/autopartial-less.js
@@ -1,6 +1,6 @@
 import * as _ from "./lib/atomic/core.js";
 
-//Arrow functions help, but are noisy and not point-free.
+//Arrow functions are noisy and not point free.  Yuck!
 _.chain(_.range(10),
   x => _.map(_.pipe(y => _.str("Number ", y), _.lowerCase), x),
   x => _.join(", ", x),
