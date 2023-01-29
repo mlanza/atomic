39a424b866b67858b091c1e2ff3280f79c4b82ad
Mario T. Lanza <me@mlanza.com>
2023-01-29 13:00:12 -0500

    standardize default flatMap

--- src/core/protocols/iflatmappable/instance.js
+++ src/core/protocols/iflatmappable/instance.js
@@ -1,5 +1,12 @@
 import {protocol} from "../../types/protocol.js";
+import {chain} from "../../core.js";
+import {IFunctor} from "../ifunctor/instance.js";
+
+function flatMap(self, f){
+  return chain(self, IFlatMappable.flat, IFunctor.fmap(?, f));
+}
+
 export const IFlatMappable = protocol({
   flat: null,
-  flatMap: null
+  flatMap
 });
