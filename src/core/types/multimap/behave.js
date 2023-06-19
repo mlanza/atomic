ba3bb9dfb051fed9f221d46840028cfe9509e0ea
Mario T. Lanza <me@mlanza.com>
2023-06-19 16:34:03 -0400

    refine multimap

--- src/core/types/multimap/behave.js
+++ src/core/types/multimap/behave.js
@@ -1,18 +1,14 @@
 import {does, comp} from "../../core.js";
 import {implement} from "../protocol.js";
-import {ISeq, ICoercible, IReducible, IKVReducible, ICounted, ISeqable, ICollection, ILookup, IMap, IAssociative} from "../../protocols.js";
+import {ISeq, ICoercible, IReducible, IKVReducible, ISeqable, ICollection, ILookup, IMap, IAssociative} from "../../protocols.js";
 import {map, concatenated} from "../lazy-seq.js";
 import {emptyList} from "../empty-list/construct.js";
 import record from "../record/behave.js";
 import * as p from "./protocols.js";
 import {keying} from "../../protocols/imapentry/concrete.js";
 
-function keys(self){
-  return Object.keys(self.attrs);
-}
-
-function count(self){
-  return p.count(seq(self));
+function lookup(self, key){
+  return self[key] || [];
 }
 
 function seq(self){
@@ -20,28 +16,13 @@ function seq(self){
     return map(function(value){
       return [key, value];
     }, p.seq(p.get(self, key)) || emptyList());
-  }, keys(self)));
-}
-
-function first(self){
-  return p.first(seq(self));
-}
-
-function rest(self){
-  return p.rest(seq(self));
-}
-
-function lookup(self, key){
-  return p.get(self.attrs, key);
+  }, p.keys(self)));
 }
 
 function assoc(self, key, value){
-  const values = lookup(self, key) || self.empty(key);
-  return new self.constructor(p.assoc(self.attrs, key, p.conj(values, value)), self.empty);
-}
-
-function contains(self, key){
-  return p.contains(self.attrs, key);
+  const copy = p.clone(self);
+  copy[key] = p.conj(lookup(self, key), value);
+  return copy;
 }
 
 function reduce(self, f, init){
@@ -58,12 +39,8 @@ function reducekv(self, f, init){
 
 export default does(
   record,
-  keying("Multimap"),
-  implement(IMap, {keys}),
   implement(IReducible, {reduce}),
   implement(IKVReducible, {reducekv}),
-  implement(ICounted, {count}),
   implement(ISeqable, {seq}),
   implement(ILookup, {lookup}),
-  implement(IAssociative, {assoc, contains}),
-  implement(ISeq, {first, rest}));
+  implement(IAssociative, {assoc}));
