c7364826d0d2e3e0be8e91816c6e3999f118a061
Mario T. Lanza <me@mlanza.com>
2024-03-20 11:35:23 -0400

    edit comments

--- tests/autopartial.js
+++ tests/autopartial.js
@@ -1,14 +1,14 @@
-import _ from "./lib/atomic_/core.js";  //shadow modules have a trailing underscore...
+import _ from "./lib/atomic_/core.js";  //shadow modules (note the trailing underscore)...
 
-//...and exist to export functions imbued with partial applicability.  I call this technique autopartial.  It affords point-free style without a build step.
+//...exist to export functions imbued with partial applicability (i.e. autopartial).  It avails the point-free style without a build step.
 
 _.chain(_.range(10), //`chain` facilitates threading
   _.map(_.pipe(_.str("Number ", _), _.lowerCase), _),
   _.join(", ", _),
   _.log);
 
-//The following is ideal but requires pipe operators and partial application syntax which do not yet exist without a build step:
+//This is ideal but impossible without a build step since pipeline operators and partial application syntax have not yet landed in JS proper:
 //_.range(10)
-//  |> _.map(_.pipe(_.str("Number ", ?), _.lowerCase), ?),
-//  |> _.join(", ", ?),
+//  |> _.map(_.pipe(_.str("Number ", ?), _.lowerCase), ?)
+//  |> _.join(", ", ?)
 //  |> _.log;
