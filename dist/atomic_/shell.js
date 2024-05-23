85d75541dbcf3e659c076bc19d052024851ed1e5
Mario T. Lanza <me@mlanza.com>
2024-05-23 18:39:27 -0400

    fix export

--- dist/atomic_/shell.js
+++ dist/atomic_/shell.js
@@ -1,3 +1,3 @@
 import _ from "./core.js";
-import * as sh from "../atomic/shell.js";
-export default _.impart(sh, _.partly);
+import * as $ from "../atomic/shell.js";
+export default _.impart($, _.partly);
