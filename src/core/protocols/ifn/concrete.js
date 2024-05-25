895b5447dd0c425fa2dd536a6d419d11f349ea01
Mario T. Lanza <me@mlanza.com>
2024-05-25 18:53:34 -0400

    drop invokable

--- src/core/protocols/ifn/concrete.js
+++ src/core/protocols/ifn/concrete.js
@@ -1,23 +1,2 @@
-import {constantly, partial, doto} from "../../core.js";
-import {specify} from "../../types/protocol/concrete.js";
 import {IFn} from "./instance.js";
-import {IDeref} from "../ideref/instance.js";
-import {ISwappable} from "../iswappable/instance.js";
 export const invoke = IFn.invoke;
-
-export function invokable(obj){
-  let state = obj;
-  function invoke(self, ...args){
-    return IFn.invoke(state, ...args);
-  }
-  function swap(self, f){
-    state = f(state);
-  }
-  function deref(self){
-    return state;
-  }
-  return doto(partial(invoke, null),
-    specify(IFn, {invoke}),
-    specify(ISwappable, {swap}),
-    specify(IDeref, {deref}));
-}
