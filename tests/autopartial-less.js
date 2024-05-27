bb38f0d70bb790d48868f4c2075bda9d734ef9e8
Mario T. Lanza <me@mlanza.com>
2024-05-27 09:08:55 -0400

    add placeholder partial and interactive dev pages

--- tests/autopartial-less.js
+++
@@ -1,7 +0,0 @@
-import * as _ from "./libs/atomic/core.js";
-
-//Arrow functions are noisy and not point free.  Yuck!
-_.chain(_.range(10),
-  x => _.map(_.pipe(y => _.str("Number ", y), _.lowerCase), x),
-  x => _.join(", ", x),
-  _.log);
