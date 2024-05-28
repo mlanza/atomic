1d370c8f10bf4e0c9b905f2276c1def461b62a73
Mario T. Lanza <mlanza@doesideas.com>
2024-05-28 16:23:17 -0400

    export standard realm behaviors

--- src/dom/index.js
+++ src/dom/index.js
@@ -12,6 +12,7 @@ export * from "./protocols.js";
 export * from "./protocols/concrete.js";
 export * from "./shared.js";
 export {append, prepend, before, after, omit, empty} from "atomic/shell"; //TODO is reexporting a good idea?
+
 import {behaviors} from "./behaviors.js";
 export * from "./behaviors.js";
 export const behave = _.behaves(behaviors, ?);
