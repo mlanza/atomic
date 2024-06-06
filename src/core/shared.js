cb03aaf3dbdd64237e25d4d85cd75a71e8b7d6bc
Mario T. Lanza <mario@doesideas.com>
2024-06-06 17:03:48 -0400

    add sequence

--- src/core/shared.js
+++ src/core/shared.js
@@ -1,15 +1,34 @@
+import {identity, unspread, comp, overload} from "./core.js";
 import {isReduced, reduced, unreduced} from "./types/reduced.js";
 import * as s from "./protocols/iseq/concrete.js"
+import {conj} from "./protocols/icollection/concrete.js";
 import {seq} from "./protocols/iseqable/concrete.js";
 import {get} from "./protocols/ilookup/concrete.js";
 import {keys} from "./protocols/imap/concrete.js";
 import {lazySeq} from "./types/lazy-seq/construct.js";
-import {mapIndexed} from "./types/lazy-seq/concrete.js";
+import {map, mapIndexed} from "./types/lazy-seq/concrete.js";
 import {cons} from "./types/list/construct.js";
 import {emptyList} from "./types/empty-list/construct.js";
 import {array} from "./types/array/construct.js";
 
 export const seqIndexed = mapIndexed(array, ?);
+
+const sequence1 = map(identity, ?);
+
+function sequence2(xform, coll){
+  return seq(coll) ? lazySeq(function(){
+    const step = xform(function(memo, value){
+      return cons(value, memo);
+    });
+    return step(sequence2(xform, s.rest(coll)), s.first(coll));
+  }) : emptyList();
+}
+
+function sequence3(xform, ...colls){
+  return sequence2(xform, map(unspread(identity), ...colls));
+}
+
+export const sequence = overload(null, sequence1, sequence2, sequence3);
 
 export function first(self){
   const key = s.first(keys(self));
