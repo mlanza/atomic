9f597a727364d9e6cf6d87324c68ae84f8f63f26
Mario T. Lanza <me@mlanza.com>
2024-07-05 19:11:04 -0400

    add equals policy to map/set

--- src/core/types/persistent-map/construct.js
+++ src/core/types/persistent-map/construct.js
@@ -1,13 +1,15 @@
 import {IAssociative} from "../../protocols/iassociative/instance.js";
+import {equiv} from "../../protocols/iequiv/concrete.js";
 
-export function PersistentMap(mapped, length){
+export function PersistentMap(mapped, length, equals){
   this.mapped = mapped;
   this.length = length;
+  this.equals = equals;
 }
 
-export function persistentMap(xs = []){
+export function persistentMap(xs = [], equals = equiv){
   const entries = Array.from(xs);
-  let map = new PersistentMap({}, 0);
+  let map = new PersistentMap({}, 0, equals);
   for(const [key, value] of entries){
     map = IAssociative.assoc(map, key, value);
   }
--- src/core/types/persistent-map/protocols.js
+++ src/core/types/persistent-map/protocols.js
@@ -3,7 +3,6 @@ export * from "../../protocols/icollection/concrete.js";
 export * from "../../protocols/ilookup/concrete.js";
 export * from "../../protocols/icounted/concrete.js";
 export * from "../../protocols/icloneable/concrete.js";
-export * from "../../protocols/iequiv/concrete.js";
 export * from "../../protocols/imap/concrete.js";
 export * from "../../protocols/iseqable/concrete.js";
 export * from "../../protocols/iseq/concrete.js";
