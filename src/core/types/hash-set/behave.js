d5cebf48f46b87ae36ab150bf29bf4a6cefb7325
Mario T. Lanza <me@mlanza.com>
2025-03-28 18:21:30 -0400

    generalize

--- src/core/types/hash-set/behave.js
+++ src/core/types/hash-set/behave.js
@@ -2,11 +2,9 @@ import {does, overload, identity, constantly} from "../../core.js";
 import {implement} from "../protocol.js";
 import {map, mapa, into} from "../lazy-seq/concrete.js";
 import {hashSet, HashSet} from "./construct.js";
-import {hashSeq as hash} from "../../protocols/ihashable/hashers.js";
 import {reduced} from "../reduced.js";
 import {keying} from "../../protocols/imapentry/concrete.js";
-import {reduce, reducekv} from "../../shared.js";
-import {IHashable, IFunctor, IFn, ILookup, IMergable, ICloneable, IEquiv, IInclusive, ISet, ISeqable, ISeq, ICollection, ICounted, IEmptyableCollection, IReducible, IKVReducible} from "../../protocols.js";
+import {IFunctor, IFn, ILookup, IMergable, ICloneable, IInclusive, ISet, ISeqable, ISeq, ICollection, ICounted, IEmptyableCollection} from "../../protocols.js";
 import * as p from "./protocols.js";
 import behave from "../set/behave.js";
 
@@ -14,7 +12,9 @@ function clone(self){
   return new HashSet(p.clone(self.coll));
 }
 
-const empty = constantly(hashSet());
+function empty(){
+  return hashSet();
+}
 
 function disj(self, value){
   return includes(self, value) ? new HashSet(p.dissoc(self.coll, value)) : self;
@@ -40,12 +40,6 @@ function seq(self){
   return p.count(self.coll) ? map(p.first, p.seq(self.coll)) : null;
 }
 
-function equiv(self, other){
-  return count(self) === p.count(other) && reduce(self, function(memo, value){
-    return memo && p.includes(other, value) ? true : reduced(false);
-  }, true);
-}
-
 function first(self){
   return p.first(seq(self));
 }
@@ -66,14 +60,10 @@ export default does(
   implement(IMergable, {merge}),
   implement(ICollection, {conj}),
   implement(ISet, {disj}),
-  implement(IEquiv, {equiv}),
   implement(IInclusive, {includes}),
   implement(ILookup, {lookup}),
   implement(IFn, {invoke: lookup}),
   implement(IFunctor, {fmap}),
-  implement(IHashable, {hash}),
-  implement(IReducible, {reduce}),
-  implement(IKVReducible, {reducekv}),
   implement(IEmptyableCollection, {empty}),
   implement(ICloneable, {clone}),
   implement(ISeq, {first, rest}),
--- src/core/types/serial-set/behave.js
+++ src/core/types/serial-set/behave.js
@@ -2,12 +2,10 @@ import {serialSet, SerialSet} from "./construct.js"
 import {keying} from "../../protocols/imapentry/concrete.js";
 import {chain, does} from "../../core.js";
 import {maybe} from  "../just/construct.js";
-import {map, mapa} from "../lazy-seq/concrete.js";
+import {mapa} from "../lazy-seq/concrete.js";
 import {implement} from "../../types/protocol/concrete.js";
-import {reduceWith} from "../../shared.js";
-import {hashSeq as hash} from "../../protocols/ihashable/hashers.js";
 import * as p from "../../protocols/concrete.js";
-import {ICloneable, IMergable, IHashable, IFunctor, IFn, ILookup, IReducible, IInclusive, ISeqable, ICollection, ISeq, ISet, IEmptyableCollection} from "../../protocols.js";
+import {ICloneable, IMergable, IFunctor, IFn, ILookup, IInclusive, ISeqable, ICollection, ISeq, ISet, IEmptyableCollection} from "../../protocols.js";
 import behave from "../set/behave.js";
 
 function first(self){
@@ -50,8 +48,6 @@ function merge(self, other){
   return new self.constructor([...self, ...other], self.serialize);
 }
 
-const reduce = reduceWith(seq);
-
 function fmap(self, f){
   return serialSet(mapa(f, self), self.serialize);
 }
@@ -59,7 +55,6 @@ function fmap(self, f){
 export default does(
   behave,
   keying("SerialSet"),
-  implement(IReducible, {reduce}),
   implement(ISeq, {first, rest}),
   implement(IEmptyableCollection, {empty}),
   implement(ICollection, {conj}),
@@ -68,7 +63,6 @@ export default does(
   implement(ILookup, {lookup}),
   implement(IFn, {invoke: lookup}),
   implement(IFunctor, {fmap}),
-  implement(IHashable, {hash}),
   implement(IMergable, {merge}),
   implement(ICloneable, {clone}),
   implement(ISeqable, {seq}));
